aa37877dc40f9cf89cc0ade4aeda76448be2a0b0
rameesabava <rameesabava91@gmail.com>
2026-03-19 11:50:41 +0530

    changed

--- src/services/allResumeApiService.js
+++ src/services/allResumeApiService.js
@@ -2,7 +2,7 @@ import apiService from "../api/apiService";
 
 // add resume api called by UserInput 
 export const addResumeAPI = async (resumeData) => {
-    return await apiService("POST", "/allResumes", resumeData)
+    return await apiService("POST","/allResumes",resumeData)
 }
 
 // get resume api called by viewResume
